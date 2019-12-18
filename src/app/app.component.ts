b94afba2cb8b27912af64b802a8541eda185565a
doron davidowitz <doron@evisame.com>
2019-12-18 14:18:15 +0200

    mateial navigation  and font icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faWallet, faTrophy, faSign } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faWallet = faWallet;
+  faTrophy = faTrophy;
+  faSign = faSign;
   title = 'classes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    FontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
